e0bf154d1143da93558d84ffa52f560d7e3d7ed5
TonyJem <a.jemanov@gmail.com>
2021-03-10 11:14:40 +0200

    Add solution

--- Task26-2-1.playground/Contents.swift
+++ Task26-2-1.playground/Contents.swift
@@ -9,3 +9,25 @@ defaultValue.set("iOS Trainee", forKey: "MyStatus")
 defaultValue.set([6, 11, 90], forKey: "Digits")
 defaultValue.set(7.904563290346863, forKey: "LongNum")
 
+// Solution:
+
+let digit = defaultValue.integer(forKey: "Digit")
+print(digit)
+
+let isLightOn = defaultValue.bool(forKey: "LightIsOn")
+print(isLightOn)
+
+let status = defaultValue.string(forKey: "MyStatus")
+if let status = status {
+    print(status)
+}
+
+let digits = defaultValue.array(forKey: "Digits")
+if let digitsUnwarapped = digits {
+    digitsUnwarapped.forEach { digit in
+        print(digit)
+    }
+}
+
+let longNumber = defaultValue.double(forKey: "LongNum")
+print(longNumber)
