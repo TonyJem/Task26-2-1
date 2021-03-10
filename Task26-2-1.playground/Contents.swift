65c2d5e907388884889fad59e58034fb0876513e
TonyJem <a.jemanov@gmail.com>
2021-03-10 11:03:53 +0200

    Add SourceCode for Task

--- Task26-2-1.playground/Contents.swift
+++ Task26-2-1.playground/Contents.swift
@@ -1,5 +1,11 @@
 import UIKit
 
 // Task:
+let defaultValue = UserDefaults.standard
 
+defaultValue.set(8, forKey: "Digit")
+defaultValue.set(false, forKey: "LightIsOn")
+defaultValue.set("iOS Trainee", forKey: "MyStatus")
+defaultValue.set([6, 11, 90], forKey: "Digits")
+defaultValue.set(7.904563290346863, forKey: "LongNum")
 
